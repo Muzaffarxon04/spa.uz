8f191005a06a908e2096e07e741caaf557b17497
Muzaffarxon04 <muzaffarxonabdusalomov04@gmail.com>
2022-01-17 19:22:17 +0500

    add

---
+++ app.js
@@ -0,0 +1,51 @@
+let headerNav = document.querySelector('.info-header');
+let tabcontent = document.querySelectorAll('.info-tabcontent');
+let tab = document.querySelectorAll('.info-header-tab');
+
+function hidetabcontent(x) {
+    for(let i = x; i < tabcontent.length; i++) {
+        tabcontent[i].classList.remove('show')
+        tabcontent[i].classList.add('hide')
+    }
+};
+
+hidetabcontent(1, tabcontent);
+
+
+function showtabcontent(y){
+    if (tabcontent[y].classList.contains('hide')) {
+        tabcontent[y].classList.remove('hide')
+        tabcontent[y].classList.add('show')
+    }
+};
+
+headerNav.addEventListener('click', (evt) =>{
+    let target = evt.target;
+    
+    for (let i = 0; i < tab.length; i++) {
+        if (target == tab[i]){
+            hidetabcontent(0, tabcontent)
+            showtabcontent(i, tabcontent)
+            break;
+        }
+    }
+})
+
+
+
+
+var count = new Date("jan 01, 2022, 22:59:00").getTime();
+var x = setInterval(function(){
+    var now = new Date().getTime();
+    var d = count - now;
+    
+
+    var hours = Math.floor((d%(1000*60*60*24)) / (1000*60*60));
+    var minutes = Math.floor((d%(1000*60*60)) / (1000*60));
+    var seconds = Math.floor((d%(1000*60)) / 1000);
+
+
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+}, 1000)
